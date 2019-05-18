1e854788aebacbcc8296ca7fb8d77bde6eecacfc
Josh Ludahl <josh.ludahl@gmail.com>
2019-05-18 08:52:10 -0700

    Updated Logging

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+        println(randomInt)
     }
 
 
